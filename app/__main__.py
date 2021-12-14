cd79f9a80fea7527c537dff8defb9b475af937d3
Arthur Lemaire <arthur.lemaire@epitech.eu>
2021-12-14 14:35:25 +0100

    initial commit

---
+++ app/__main__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def helloIndex():
+    return 'Hello world!'
+
+
+print('Hello world!', flush=True)
+
+# app.run(host='0.0.0.0', port=8080)
